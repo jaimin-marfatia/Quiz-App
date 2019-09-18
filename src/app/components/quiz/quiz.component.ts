d82def7a07e327362ef63120beaa56592df01caf
jaimin1224 <marfatiajaimin@gmail.com>
2019-09-18 19:45:30 -0400

    - Quiz component completed

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Question } from "../../models/question";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-quiz",
@@ -8,29 +9,39 @@ import { Question } from "../../models/question";
 })
 export class QuizComponent implements OnInit {
   questions: Question[];
+  queProgress: number = 0;
 
-  constructor() {}
+  constructor(public router: Router) {}
 
   ngOnInit() {
     this.questions = [
       {
         id: 1,
-        que: "question 1",
-        option1: "this is option 1",
-        option2: "this is option 1",
-        option3: "this is option 1",
-        option4: "this is option 1",
-        answer: "correct answer"
+        que: "Which is the largest country in world by population?",
+        options: ["India", "USA", "China", "Russia"],
+        answer: "China"
       },
       {
         id: 2,
-        que: "question 2",
-        option1: "this is option 1",
-        option2: "this is option 1",
-        option3: "this is option 1",
-        option4: "this is option 1",
-        answer: "correct answer"
+        que: "When did the second world war end?",
+        options: ["1945", "1939", "1944", "1942"],
+        answer: "1945"
+      },
+      {
+        id: 3,
+        que: "Which was the first country to issue paper currency?",
+        options: ["USA", "France", "Italy", "China"],
+        answer: "China"
       }
     ];
+  }
+
+  Answer(quesId, choice) {
+    console.log(quesId, choice);
+    this.queProgress++;
+
+    if (this.queProgress === 3) {
+      this.router.navigate(["result"]);
+    }
   }
 }
--- src/app/models/question.ts
+++ src/app/models/question.ts
@@ -1,9 +1,6 @@
 export class Question {
   id: number;
   que: string;
-  option1: string;
-  option2: string;
-  option3: string;
-  option4: string;
+  options: string[];
   answer: string;
 }
