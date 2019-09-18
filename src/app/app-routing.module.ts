c59307c115de98c0659a850a426b6e8098458e46
jaimin1224 <marfatiajaimin@gmail.com>
2019-09-17 21:10:55 -0400

    Connecting project to git repo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ResultComponent } from "./components/result/result.component";
+import { QuizComponent } from "./components/quiz/quiz.component";
+import { WelcomeComponent } from "./components/welcome/welcome.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "result", component: ResultComponent },
+  { path: "quiz", component: QuizComponent },
+  { path: "welcome", component: WelcomeComponent },
+  { path: "", component: WelcomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Question } from "../../models/question";
 
 @Component({
-  selector: 'app-quiz',
-  templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.css']
+  selector: "app-quiz",
+  templateUrl: "./quiz.component.html",
+  styleUrls: ["./quiz.component.css"]
 })
 export class QuizComponent implements OnInit {
+  questions: Question[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.questions = [
+      {
+        id: 1,
+        que: "question 1",
+        option1: "this is option 1",
+        option2: "this is option 1",
+        option3: "this is option 1",
+        option4: "this is option 1",
+        answer: "correct answer"
+      },
+      {
+        id: 2,
+        que: "question 2",
+        option1: "this is option 1",
+        option2: "this is option 1",
+        option3: "this is option 1",
+        option4: "this is option 1",
+        answer: "correct answer"
+      }
+    ];
   }
-
 }
